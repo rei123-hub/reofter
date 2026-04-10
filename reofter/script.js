f906fb94ae462d235542c168bc38b34e8d466efc
rei123-hub <rivanmusawa7@gmail.com>
2026-04-11 01:08:19 +0700

    Update script.js

--- reofter/script.js
+++ reofter/script.js
@@ -3,6 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const gallery = document.getElementById("gallery");
     const btnLeft = document.querySelector(".left");
     const btnRight = document.querySelector(".right");
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modalImg");
+    const closeBtn = document.querySelector(".close");
+
+
+    function getCardWidth() {
+        const card = gallery.querySelector(".card");
+        const style = window.getComputedStyle(card);
+        const margin = parseInt(style.marginRight) || 0;
+        return card.offsetWidth + margin;
+    }
 
     function updateTombol() {
         if (gallery.scrollLeft <= 0) {
@@ -21,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // tombol kanan
     window.geserKanan = function () {
         gallery.scrollBy({
-            left: 220,
+            left: getCardWidth(),
             behavior: "smooth"
         });
         setTimeout(updateTombol, 300);
@@ -30,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // tombol kiri
     window.geserKiri = function () {
         gallery.scrollBy({
-            left: -220,
+            left: -getCardWidth(),
             behavior: "smooth"
         });
         setTimeout(updateTombol, 300);
@@ -38,6 +49,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     gallery.addEventListener("scroll", updateTombol);
 
-    updateTombol(); // langsung cek saat load
+    updateTombol();
+
+    document.querySelectorAll(".card img").forEach(img => {
+        img.addEventListener("click", function () {
+            modal.classList.add("show");
+            modalImg.src = this.src;
+        });
+    });
+
+    function closeModal() {
+        modal.classList.remove("show");
+    }
+
+    closeBtn.onclick = closeModal;
+
+    modal.onclick = function (e) {
+        if (e.target === modal) {
+            closeModal();
+        }
+    };
 
 });
